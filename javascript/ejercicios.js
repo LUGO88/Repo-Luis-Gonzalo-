785a01be5d0ec09b4e052f0c783d1c6e480a9d50
Luis Gonzalo Alvarez <202260313@ucc.mx>
2025-04-02 19:02:18 -0600

    Progress bar y Login

---
+++ javascript/ejercicios.js
@@ -0,0 +1,35 @@
+/*var respuesta = confirm("¿Estás seguro(a) de continuar?");
+if (respuesta == true) {
+    continuar();
+}else {
+    cancelar();
+}*/
+
+/*
+var respuesta = prompt("¿Cuál es tu nombre?", "Feliz día!!!");
+if (respuesta != null) {
+    continuar(respuesta);
+}else {
+    cancelar();
+}*/
+
+/*
+setInterval("Mensaje()", 5000);
+function Mensaje() {
+    alert("Hola mundo");
+}
+*/
+
+//anonima
+/*
+setInterval(function() {
+    alert("Hola mundo");
+}, 5000);
+*/
+
+/*
+setTimeout("Mensaje()", 5000);
+function Mensaje() {
+    alert("Hola mundo");
+}*/
+
